d0ce1cacf128e7ca293faef990823f1ddde9abf3
Steve Lipton <stevelipton@brk-16892.local>
2019-01-07 07:18:54 -0600

    Start state for the Range Demo lesson

---
+++ RangePlayground.playground/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+//:# Range Demo
+//
+//:  A Demo for iOS Development Tips Weekly
+//:  by Steven Lipton (C)2018, All rights reserved
+//:  For videos go to http://bit.ly/TipsLinkedInLearning
+//:  For code go to http://bit.ly/AppPieGithub
+//:
+
+let myDesserts = ["Cannoli","Mochi","Bunelos","Pecan Pie","Ice Cream"]
+
+//: Closed range
+var total = 0
+for number in 0...5{
+    total += number
+}
+total
+
+//: Half Open range
+total = 0
+for number in 0..<5{
+    total += number
+}
+total
+
+
+//: Assigning ranges
+
+
+
+
+//: Use as array subscripts
+
+
+//: Membership
+
+
+
+
+
+
+
+
+
+
+
+
